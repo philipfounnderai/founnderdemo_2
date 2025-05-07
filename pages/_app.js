129207315abbe6271672b7f33c221dafbc57bcd6
Philip M. Musey Jr <philipmusey@macbookpro.mynetworksettings.com>
2025-05-07 02:26:26 -0400

    Scroll to top on page load and route change

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,7 @@
 import { Inter } from 'next/font/google';
 import "@/styles/globals.css";
+import { useEffect } from "react";
+import { useRouter } from "next/router";
 
 const inter = Inter({
   subsets: ['latin'],
@@ -7,6 +9,12 @@ const inter = Inter({
 });
 
 export default function App({ Component, pageProps }) {
+  const router = useRouter();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [router.asPath]);
+
   return (
     <main className={inter.className}>
       <Component {...pageProps} />
